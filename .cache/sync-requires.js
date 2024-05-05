5f7e39a20f4d38c2c9a4aa951424d33a7c3f764a
ChetaniyaBajaj <chetaniyabajaj@gmail.com>
2024-05-05 16:28:50 +0530

    some changes

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-caches-gatsby-plugin-offline-app-shell-js": preferDefault(require("/home/chetan/Temp 2/.cache/caches/gatsby-plugin-offline/app-shell.js")),
+  "component---cache-dev-404-page-js": preferDefault(require("/home/chetan/Temp 2/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/home/chetan/Temp 2/src/pages/404.js")),
   "component---src-pages-archive-js": preferDefault(require("/home/chetan/Temp 2/src/pages/archive.js")),
   "component---src-pages-index-js": preferDefault(require("/home/chetan/Temp 2/src/pages/index.js")),
